c375c26eeb17203d99ff024d3bac921267559ac7
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-02-16 01:05:45 +0300

    [base] add login page

---
+++ sodfu-court-db/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import "primereact/resources/themes/mdc-light-indigo/theme.css";
+import './App.css';
+import Login from './Components/Login/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
